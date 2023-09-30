067c2bea1e3ff2709bc16fd1326fefb89ab7e274
yagizpancak <yagizpancak@gmail.com>
2023-10-01 02:03:48 +0300

    update getShipmentSize

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -3,16 +3,46 @@ package com.trendyol.shipment;
 import com.trendyol.shipment.exception.BasketEmptyException;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class Basket {
 
+    private static final int SIZE_THRESHOLD = 3;
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        if (products.isEmpty()) {
+        if (products == null || products.isEmpty()) {
             throw new BasketEmptyException();
         }
-        return null;
+
+        Optional<ShipmentSize> sameSizeExceedThreshold = getSameSizeExceedThreshold();
+
+        if (sameSizeExceedThreshold.isPresent()){
+            return sameSizeExceedThreshold.get().getNextSize();
+        }
+
+        return findLargestSizeProduct();
+    }
+
+    private Optional<ShipmentSize> getSameSizeExceedThreshold() {
+        return products.stream()
+                .collect(groupingBy(Product::getSize, counting()))
+                .entrySet()
+                .stream()
+                .filter(sizeEntry -> sizeEntry.getValue() >= SIZE_THRESHOLD)
+                .map(Map.Entry::getKey)
+                .findFirst();
+    }
+
+    private ShipmentSize findLargestSizeProduct() {
+        return products.stream()
+                .map(Product::getSize)
+                .max(ShipmentSize::compareTo)
+                .orElse(null);
     }
 
     public List<Product> getProducts() {
--- src/main/java/com/trendyol/shipment/Product.java
+++ src/main/java/com/trendyol/shipment/Product.java
@@ -4,10 +4,12 @@ public class Product {
 
     private ShipmentSize size;
 
+    public Product(ShipmentSize size) {
+        this.size = size;
+    }
+
     public static Product create(ShipmentSize shipmentSize) {
-        Product productVO = new Product();
-        productVO.setSize(shipmentSize);
-        return productVO;
+        return new Product(shipmentSize);
     }
 
     public ShipmentSize getSize() {
