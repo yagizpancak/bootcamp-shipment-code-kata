1a3d83138497789e0e98954feda68ea5a387a1bd
yagizpancak <yagizpancak@gmail.com>
2023-09-30 19:26:41 +0300

    add getNextSize() to ShipmentSize

--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -5,5 +5,15 @@ public enum ShipmentSize {
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+
+    private static final ShipmentSize[] values = ShipmentSize.values();
+    private final int ordinal = this.ordinal();
+
+    public ShipmentSize getNextSize() {
+        if (ordinal == values.length - 1){
+            return this;
+        }
+        return values[ordinal + 1];
+    }
 }
